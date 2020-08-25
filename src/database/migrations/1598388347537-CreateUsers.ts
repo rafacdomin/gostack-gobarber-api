d5d86a97b2d628953b26970619baba358dbca9a6
Rafael <rafaelcodomingues@gmail.com>
2020-08-25 18:16:35 -0300

    :rocket: Create appointments and user relationship

--- src/database/migrations/1598388347537-CreateUsers.ts
+++ src/database/migrations/1598388347537-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598388347537 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -3,7 +3,11 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -11,7 +15,11 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
