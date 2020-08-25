844036ccdb0ca134bf015e2027c952420746cebd
Rafael <rafaelcodomingues@gmail.com>
2020-08-25 17:58:43 -0300

    :rocket: Create users table

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,24 +3,28 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
