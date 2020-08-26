cedc2894d86cbbd70875752e3df67a1c51909324
Rafael <rafaelcodomingues@gmail.com>
2020-08-26 17:32:09 -0300

    :rocket: Create sessions routes

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: RequestDTO): Promise<Response> {
+    const usersRepo = getRepository(User);
+
+    const user = await usersRepo.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('User not found,please verify your credentials');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('User not found,please verify your credentials');
+    }
+
+    delete user.password;
+
+    const token = sign({}, 'c506adb48e677730ed6e3dc08d488598', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
