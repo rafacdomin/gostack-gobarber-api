8884c0d2f327dde525899f2c8b066bb4933db520
Rafael <rafaelcodomingues@gmail.com>
2020-08-26 18:51:48 -0300

    :rocket: Add avatar static route

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const server = express();
 
 server.use(express.json());
+server.use('/files', express.static(uploadConfig.directory));
 server.use(routes);
 
 server.listen(3333);
