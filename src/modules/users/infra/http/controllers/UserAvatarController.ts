4b9f44d8230053d32b43d9aaee3eeb224cb508e8
Rafael <rafaelcodomingues@gmail.com>
2020-10-05 18:03:46 -0300

    :tada: Add class transformer to hide password

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -12,6 +13,6 @@ export default class UserAvatarController {
       avatarFilename: req.file.filename,
     });
 
-    return res.json(user);
+    return res.json(classToClass(user));
   }
 }
